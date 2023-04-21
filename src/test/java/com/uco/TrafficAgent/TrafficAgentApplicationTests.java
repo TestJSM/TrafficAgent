96c9cb3ee7b483a732ebf136e225e56d327e2857
TestJSM <131490774+TestJSM@users.noreply.github.com>
2023-04-21 17:41:26 -0500

    Update TrafficAgentApplicationTests.java

--- src/test/java/com/uco/TrafficAgent/TrafficAgentApplicationTests.java
+++ src/test/java/com/uco/TrafficAgent/TrafficAgentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TrafficAgentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
